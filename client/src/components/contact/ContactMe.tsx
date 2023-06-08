579cab2164cb7658fc83b2aeb9eade3b4e747a76
Ako Karimi <ako.karimii@gmail.com>
2023-06-08 17:41:10 +0330

    optimize images

--- client/src/components/contact/ContactMe.tsx
+++ client/src/components/contact/ContactMe.tsx
@@ -1,18 +1,13 @@
-import { useInView } from "react-intersection-observer";
+import React from "react";
 
 const ContactMe = () => {
-  const [ref, inView] = useInView();
   return (
-    <section ref={ref} className="bg-light1 dark:bg-dark overview-x-hidden">
+    <section className="bg-light1 dark:bg-dark">
       <div className="container mx-auto pt-20 pb-16 md:pt-24 md:pb-14 gap-2 ">
         <p className="text-xl md:text-4xl lg:text-5xl text-dark1 dark:text-light1 font-bold pb-12">
-          Contact Me...
+          Contact Me
         </p>
-        <div
-          className={`text-sm md:text-md text-dark1 dark:text-light1 grid grid-cols-[1fr_2fr] gap-4 justify-start border border-gray-200 dark:border-none rounded-2xl shadow-2xl p-4 bg-white dark:bg-dark1 transition-all duration-1000 ${
-            inView ? "translate-x-0" : "translate-x-[200%]"
-          }`}
-        >
+        <div className="text-sm md:text-md text-dark1 dark:text-light1 grid grid-cols-[1fr_2fr] gap-4 justify-start border border-gray-200 dark:border-none rounded-2xl shadow-2xl p-4 bg-white dark:bg-dark1">
           <p className="font-bold flex gap-1 items-center">
             <svg className="w-6 h-4 md:w-6 fill-primary">
               <use xlinkHref="img/svg-icons.svg#icon-mobile2"></use>
