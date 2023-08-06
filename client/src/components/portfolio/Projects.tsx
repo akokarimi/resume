4edc68f94a45f4d95908ea564717a11c0a23d949
Ako Karimi <ako.karimii@gmail.com>
2023-08-06 22:55:24 +0330

    add new project

--- client/src/components/portfolio/Projects.tsx
+++ client/src/components/portfolio/Projects.tsx
@@ -19,6 +19,53 @@ function Projects() {
             transition-all duration-1000 ${inView1 ? "opacity-1" : "opacity-0"}
           `}
           >
+            <img
+              src="/img/haylo.png"
+              className="max-w-[8rem] max-h-[8rem] dark:invert"
+              alt="haylo logo"
+            />
+
+            <div className="flex flex-col gap-4">
+              <div className="flex flex-col gap-1 text-center">
+                <p className="font-bold text-dark1 dark:text-light1 mb-2 mt-4">
+                  Name:
+                </p>
+                <p className="text-dark2 dark:text-light2">Haylo Flower Shop</p>
+              </div>
+              <div className="flex flex-col gap-1 text-center">
+                <p className="font-bold text-dark1 dark:text-light1 mb-2 mt-4">
+                  Languages and Platforms Used:
+                </p>
+                <p className="text-dark2 dark:text-light2">
+                  NextJs, TypeScript, MongoDB
+                </p>
+              </div>
+              <div className="flex flex-col gap-1 text-center">
+                <p className="font-bold text-dark1 dark:text-light1 mb-2 mt-4">
+                  Features:
+                </p>
+                <p className="text-dark2 dark:text-light2">
+                  Signing Up With Email,
+                  <br />
+                  Adding products to cart,
+                </p>
+              </div>
+            </div>
+            <a
+              href="https://haylo.vercel.app"
+              target="_blank"
+              rel="noreferrer"
+              className="my-8 text-dark1 dark:text-light1 border-2 border-primary w-36 md:w-40 py-2 px-6 md:px-8 text-center shadow-md rounded-3xl hover:bg-primary hover:text-white hover:border-primary transition duration-500"
+            >
+              Visit Website
+            </a>
+          </div>
+          <div
+            ref={ref1}
+            className={`grid grid-rows-[96px_1fr] gap-5 w-56 xs:w-72 h-[610px] justify-items-center border border-gray-200 dark:border-none rounded-2xl shadow-2xl p-4 bg-white dark:bg-dark hover:scale-[1.025]
+            transition-all duration-1000 ${inView1 ? "opacity-1" : "opacity-0"}
+          `}
+          >
             <svg className="w-48">
               <use xlinkHref="img/svg-icons.svg#digikala"></use>
             </svg>
